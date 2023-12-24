ed07a4e77b387609d02347c0f92797addc951d1e
jodhsinghnahal <nahal.jodhsingh@gmail.com>
2023-12-24 00:05:43 -0800

    test connection

--- rasp.py
+++ rasp.py
@@ -1,10 +1,17 @@
 import serial
+import time
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+def main():
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    time.sleep(3)
     ser.reset_input_buffer()
+    print("SERIAL OK")
 
     while True:
+        time.sleep(0.01)
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
+
+if __name__ == "__main__":
+    main()
